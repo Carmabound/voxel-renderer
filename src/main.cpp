44750ffe7b845dcd13aaf9a3c29f47b07fa07dfe
reaganlittle <raygelbaygel@gmail.com>
2023-09-13 08:52:19 -0400

    fixed error

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 //
 // Created by Reagan Little on 9/11/23.
 //
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
-//#include <glad/glad.h>
 #include <iostream>
 
 void contextClues() {
@@ -22,33 +22,5 @@ void contextClues() {
 
 
 int main() {
-    // Initialize GLFW
-    if (!glfwInit()) {
-        std::cerr << "Failed to initialize GLFW" << std::endl;
-        return -1;
-    }
-    contextClues();
-
-    //glfwCreateWindow(640, 480, "Hello World", NULL, NULL );
-    // Create a GLFW window (even though we won't render to it)
-    GLFWwindow *window = glfwCreateWindow(1, 1, "OpenGL Test", nullptr, nullptr);
-    if (!window) {
-        std::cerr << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-
-    // Make the OpenGL context current
-    glfwMakeContextCurrent(window);
-
-    // Initialize and print OpenGL version
-    const char *glVersion = reinterpret_cast<const char *>(glGetString(GL_VERSION));
-    if (glVersion) {
-        std::cout << "OpenGL Version: " << glVersion << std::endl;
-    } else {
-        std::cerr << "Failed to retrieve OpenGL version" << std::endl;
-    }
-
-    // Terminate GLFW
 
 }
